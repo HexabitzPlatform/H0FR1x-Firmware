a77863a1c4a4cbce0e773e0c637e58e6c031af72
AhmadAbdulhai\AHMAD <ahmad52585258@gmail.com>
2024-09-26 16:29:50 +0300

    Edit PIN & PORT UART and comment unused UART

--- H0FR1/H0FR1.h
+++ H0FR1/H0FR1.h
@@ -41,29 +41,29 @@
 #define _P3
 #define _P4
 #define _P5
-#define _P6
+//#define _P6
 
 /* Define available USARTs */
 #define _Usart1 1
 #define _Usart2 1
 #define _Usart3 1
-#define _Usart4	1
+//#define _Usart4	1
 #define _Usart5 1
 #define _Usart6	1
 
 /* Port-UART mapping */
-#define P1uart &huart4
+#define P1uart &huart6
 #define P2uart &huart2
-#define P3uart &huart6
+#define P3uart &huart5
 #define P4uart &huart3
-#define P5uart &huart5
-#define P6uart &huart1
+#define P5uart &huart1
+//#define P6uart &huart1
 
 /* Port Definitions */
-#define	USART1_TX_PIN		GPIO_PIN_9
-#define	USART1_RX_PIN		GPIO_PIN_10
-#define	USART1_TX_PORT		GPIOA
-#define	USART1_RX_PORT		GPIOA
+#define	USART1_TX_PIN		GPIO_PIN_6
+#define	USART1_RX_PIN		GPIO_PIN_7
+#define	USART1_TX_PORT		GPIOB
+#define	USART1_RX_PORT		GPIOB
 #define	USART1_AF			GPIO_AF1_USART1
 
 #define	USART2_TX_PIN		GPIO_PIN_2
@@ -78,16 +78,16 @@
 #define	USART3_RX_PORT		GPIOB
 #define	USART3_AF			GPIO_AF4_USART3
 
-#define	USART4_TX_PIN		GPIO_PIN_0
-#define	USART4_RX_PIN		GPIO_PIN_1
-#define	USART4_TX_PORT		GPIOA
-#define	USART4_RX_PORT		GPIOA
-#define	USART4_AF			GPIO_AF4_USART4
+//#define	USART4_TX_PIN		GPIO_PIN_0
+//#define	USART4_RX_PIN		GPIO_PIN_1
+//#define	USART4_TX_PORT		GPIOA
+//#define	USART4_RX_PORT		GPIOA
+//#define	USART4_AF			GPIO_AF4_USART4
 
-#define	USART5_TX_PIN		GPIO_PIN_3
-#define	USART5_RX_PIN		GPIO_PIN_2
-#define	USART5_TX_PORT		GPIOD
-#define	USART5_RX_PORT		GPIOD
+#define	USART5_TX_PIN		GPIO_PIN_0
+#define	USART5_RX_PIN		GPIO_PIN_1
+#define	USART5_TX_PORT		GPIOB
+#define	USART5_RX_PORT		GPIOB
 #define	USART5_AF			GPIO_AF3_USART5
 
 #define USART6_TX_PIN   GPIO_PIN_8
@@ -141,7 +141,7 @@ extern UART_HandleTypeDef huart6;
 extern void MX_USART1_UART_Init(void);
 extern void MX_USART2_UART_Init(void);
 extern void MX_USART3_UART_Init(void);
-extern void MX_USART4_UART_Init(void);
+//extern void MX_USART4_UART_Init(void);
 extern void MX_USART5_UART_Init(void);
 extern void MX_USART6_UART_Init(void);
 extern void SystemClock_Config(void);
